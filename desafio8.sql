1a8d7c78d062a8833eae894439247bd31e88e8ba
Joiscilene <joisci.lene@hotmail.com>
2022-06-07 07:53:24 -0300

    Adicionando correção 6 e 8

--- desafio8.sql
+++ desafio8.sql
@@ -2,9 +2,10 @@ SELECT
   A.artist_name AS 'artista',
   AB.album_name AS 'album'
 FROM
-  SpotifyClone.albums AS AB,
+  SpotifyClone.albums AS AB
+INNER JOIN  
   SpotifyClone.artists AS A
 WHERE AB.artist_id = A.artist_id
-AND A.artist_name = 'Walter Phoenix' 
+AND A.artists = 'Walter Phoenix' 
 GROUP BY artists, albums
 ORDER BY albums; 
